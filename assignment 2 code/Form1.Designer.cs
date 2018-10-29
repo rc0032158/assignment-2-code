bb968b1a063938d42ec57c84f38ce091572055b4
RC0032158 <RC0032158@CRO-24-07>
2018-10-29 15:42:27 +0000

    start comments

--- assignment 2 code/Form1.Designer.cs
+++ assignment 2 code/Form1.Designer.cs
@@ -28,13 +28,163 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.t1 = new System.Windows.Forms.TextBox();
+            this.t2 = new System.Windows.Forms.TextBox();
+            this.t3 = new System.Windows.Forms.TextBox();
+            this.t5 = new System.Windows.Forms.TextBox();
+            this.t4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(266, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter a type of Circuit:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(369, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Enter the values of the resistors in the circuit, entering a number in every box:" +
+    "";
+            // 
+            // t1
+            // 
+            this.t1.Location = new System.Drawing.Point(441, 339);
+            this.t1.Name = "t1";
+            this.t1.Size = new System.Drawing.Size(93, 20);
+            this.t1.TabIndex = 3;
+            // 
+            // t2
+            // 
+            this.t2.Location = new System.Drawing.Point(441, 106);
+            this.t2.Name = "t2";
+            this.t2.Size = new System.Drawing.Size(93, 20);
+            this.t2.TabIndex = 4;
+            // 
+            // t3
+            // 
+            this.t3.Location = new System.Drawing.Point(663, 206);
+            this.t3.Name = "t3";
+            this.t3.Size = new System.Drawing.Size(93, 20);
+            this.t3.TabIndex = 5;
+            // 
+            // t5
+            // 
+            this.t5.Location = new System.Drawing.Point(317, 106);
+            this.t5.Name = "t5";
+            this.t5.Size = new System.Drawing.Size(93, 20);
+            this.t5.TabIndex = 6;
+            // 
+            // t4
+            // 
+            this.t4.Location = new System.Drawing.Point(551, 106);
+            this.t4.Name = "t4";
+            this.t4.Size = new System.Drawing.Size(93, 20);
+            this.t4.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(657, 374);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Total Resistance:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(654, 337);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Series",
+            "Parallel"});
+            this.comboBox1.Location = new System.Drawing.Point(378, 31);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 14;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(749, 375);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "label4";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::assignment_2_code.Properties.Resources.res;
+            this.pictureBox1.Location = new System.Drawing.Point(259, 132);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(398, 201);
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.t4);
+            this.Controls.Add(this.t5);
+            this.Controls.Add(this.t3);
+            this.Controls.Add(this.t2);
+            this.Controls.Add(this.t1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox t1;
+        private System.Windows.Forms.TextBox t2;
+        private System.Windows.Forms.TextBox t3;
+        private System.Windows.Forms.TextBox t5;
+        private System.Windows.Forms.TextBox t4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- assignment 2 code/Form1.cs
+++ assignment 2 code/Form1.cs
@@ -16,5 +16,136 @@ namespace assignment_2_code
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+     
+        }
+        //5 resistors in series
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                
+                double r1, r2, r3, r4, r5;
+
+                try
+                {
+                    r1 = Convert.ToDouble(t1.Text);
+                }
+                catch
+                {
+                    r1 = 0;
+                }
+
+                try
+                {
+                    r2 = Convert.ToDouble(t2.Text);
+                }
+                catch
+                {
+                    r2 = 0;
+                }
+
+                try
+                {
+                    r3 = Convert.ToDouble(t3.Text);
+                }
+                catch
+                {
+                    r3 = 0;
+                }
+
+
+                try
+                {
+                    r4 = Convert.ToDouble(t4.Text);
+                }
+                catch
+                {
+                    r4 = 0;
+                }
+                //
+
+                try
+                {
+                    r5 = Convert.ToDouble(t5.Text);
+                }
+                catch
+                {
+                    r5 = 0;
+                }
+
+
+                double rt = r1 + r2 + r3 + r4 + r5;
+
+                label4.Text = Convert.ToString(rt);
+
+
+
+            }
+            else
+            {
+                double r1, r2, r3, r4, r5;
+                double rtr = 0;
+                //5 resistors in parallel
+                try
+                {
+                    r1 = Convert.ToDouble(t1.Text);
+                    rtr = rtr + 1 / r1;
+                }
+                catch
+                {
+                    
+                }
+
+                try
+                {
+                    r2 = Convert.ToDouble(t2.Text);
+                    rtr = rtr + 1 / r2;
+                }
+                catch
+                {
+                    r2 = 0;
+                }
+
+                try
+                {
+                    r3 = Convert.ToDouble(t3.Text);
+                    rtr = rtr + 1 / r3;
+                }
+                catch
+                {
+                    r3 = 0;
+                }
+
+
+                try
+                {
+                    r4 = Convert.ToDouble(t4.Text);
+                    rtr = rtr + 1 / r4;
+                }
+                catch
+                {
+                    r4 = 0;
+                }
+
+
+                try
+                {
+                    r5 = Convert.ToDouble(t5.Text);
+                    rtr = rtr + 1 / r5;
+                }
+                catch
+                {
+                    r5 = 0;
+                }
+
+
+                double rt = 1/rtr;
+
+                label4.Text = Convert.ToString(rt);
+            }
+        }
     }
 }
